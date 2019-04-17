200a7787aead95811d0baa9e206db01ff8ec8c03
apupyshev <alexeyalexeevichpupyshev@gmail.com>
2019-04-17 13:09:16 +0300

    deposit done

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom";
 import React from 'react';
+import { invokeScript, broadcast } from '@waves/waves-transactions'
 
 class App extends React.Component {
             constructor(props) {
@@ -22,26 +23,40 @@ class App extends React.Component {
                       txid: ''
                     }
                 };
-                this.baseUri = 'https://testnodes.wavesnodes.com/v0/';
-                this.dappadress = '3N4GgQWCckqUn1H9y8rNRyCXZPkgKTNKznT';
+                this.baseUri = 'https://testnodes.wavesnodes.com';
+                this.wavelet = 100000000;
+                this.dappaddress = '3N4GgQWCckqUn1H9y8rNRyCXZPkgKTNKznT';
                 this.explorerUrl = "https://wavesexplorer.com/testnet";
                 this.deposit = this.deposit.bind(this);
                 this.invest = this.invest.bind(this);
                 this.getFunds = this.getFunds.bind(this);
                 this.updateValue = this.updateValue.bind(this);
             }
-            updateValue(scope, key, event) {
+            updateValue(scope, key, value) {
               const newState = this.state[scope];
-              newState[key] = event.target.value;
+              newState[key] = value;
               this.setState(
                       {
                         [scope]: newState
                       }
                 );
             }
-            deposit() {
+            deposit(){
               if (window.confirm("Are you sure you wish to deposit?")) {
-                console.log(this.state.deposit);
+                  const params = {
+                      dappAddress: this.dappaddress,
+                      call: {
+                          function: "deposit",
+                          args:[]
+                      },
+                      payment: [ {amount: this.state.deposit.amount*this.wavelet, asset:null } ],
+                      chainId: 84
+                  };
+                  console.log(this.state.deposit);
+                  console.log(params);
+                  let tx = invokeScript(params, this.state.deposit.seed);
+                  this.updateValue("deposit", "txid", tx.id);
+                  broadcast(tx, this.baseUri);
               }
             }
             invest() {
@@ -52,16 +67,16 @@ class App extends React.Component {
                 return (
                     <div className="container">
                       <div className="deposit">
-                        <span>Deposit</span>
+                        <span>[Investor] Deposit</span>
                         <span>: </span>
-                        <input type="text" placeholder="Seed phrase" onChange={(e) => this.updateValue("deposit", "seed", e)}/>
-                        <input type="text" placeholder="WAVES - Amount" onChange={(e) => this.updateValue("deposit", "amount", e)}/>
+                        <input type="text" placeholder="Seed phrase" onChange={(e) => this.updateValue("deposit", "seed", e.target.value)}/>
+                        <input type="number" placeholder="WAVES - Amount" onChange={(e) => this.updateValue("deposit", "amount", e.target.value)}/>
                         <input type="submit" value="Deposit" className="submit button" onClick={this.deposit}/>
                         <a href={this.explorerUrl + "/tx/" + this.state.deposit.txid}>Transaction: {this.state.deposit.txid}</a>
                         <br/>
                       </div>
                       <div className="invest">
-                        <span>Invest</span>
+                        <span>[Investor] Invest</span>
                         <span>: </span>
                         <input type="text" placeholder="Seed phrase"/>
                         <input type="text" placeholder="WAVES - Amount"/>
@@ -71,7 +86,7 @@ class App extends React.Component {
                         <br/>
                       </div>
                       <div className="Get Funds">
-                        <span>Get Funds</span>
+                        <span>[Startup] Get Funds</span>
                         <span>: </span>
                         <input type="text" placeholder="Seed phrase"/>
                         <input type="text" placeholder="WAVES - Amount"/>
