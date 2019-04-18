b2b9f6d2f16926dae00a9482f241d3293263b78f
apupyshev <alexeyalexeevichpupyshev@gmail.com>
2019-04-18 13:19:54 +0300

    added changes with ports

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/dist'));
 app.get('/', (req, res) => {
 	res.sendFile('index.html', { root : __dirname});
 });
-app.listen(3000, () => {
+app.listen(process.env.PORT || 5000, () => {
 	console.log(__dirname);
 	console.log("Listening Port 3000");
 });
